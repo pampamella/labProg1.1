f945f849ea55b7e121bde6dcd72f73a3173eeedf
vitorepn <vitorepn11@gmail.com>
2022-03-09 22:00:47 -0300

    escrever disciplinas

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include "defines.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "escreverDisciplinas.h"
+#include "disciplinasMetodos.h"
 
 void main(){
-    
+    Disciplinas *lista = NULL;
+    inserirDisciplina(&lista,10000,"matdisc","veloso",30);
+    inserirDisciplina(&lista,10001,"sistdig","rotava",30);
+    escreverDisciplinas(&lista);
 }
 
 
